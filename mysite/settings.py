14091a811fc5bd07b42046b9a1c594701aaf41e0
Your Name <you@example.com>
2021-03-16 22:55:49 +0600

    ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
