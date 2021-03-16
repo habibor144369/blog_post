2040c9d4a4579a0f3eed891787b72397bd77265b
Your Name <you@example.com>
2021-03-16 23:47:08 +0600

    inshalla

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
